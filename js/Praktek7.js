4007264a681b166f41e27373e4f8e41069b41a06
3Nami <144736648+3Nami@users.noreply.github.com>
2023-10-31 14:47:17 +0700

    Add files via upload

---
+++ js/Praktek7.js
@@ -0,0 +1,8 @@
+alert("Assalamualaikum");
+
+const menu = document.querySelector(".navbar #menu");
+const nav = document.querySelector(".navbar .navbar-nav");
+
+menu.addEventListener("click", function () {
+  nav.classList.toggle("active");
+});
