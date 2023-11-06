64f512b53f678f7e5ca7415bffbf1b6071e46dbf
3Nami <144736648+3Nami@users.noreply.github.com>
2023-11-06 21:36:36 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Toggle class active
 const navbarNav = document.querySelector(".navbar-nav");
 // Ketika yang garis 3 di click
-document.querySelector("#versi - HP").onclick = () => {
+document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
