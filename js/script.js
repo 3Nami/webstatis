ae2d003c8982052fbe879f515cae23c869e5779a
3Nami <144736648+3Nami@users.noreply.github.com>
2023-11-08 14:07:59 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 // Toggle class active
 const navbarNav = document.querySelector(".navbar-nav");
-// Ketika yang garis 3 di click
+// Ketika yang hamburger di click
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
+
+// klik selain hamburger untuk menghilangkan nav //
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
